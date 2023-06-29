671dc06a77d0f0aff23ea2449cb59bd4fb7e2078
Juri Kato <dance.luv48@gmail.com>
2023-06-29 19:28:39 +0900

    working on challenge2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/ask', to: 'pages#ask'
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/answer', to: 'pages#answer'
 end
